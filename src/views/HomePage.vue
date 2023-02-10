c0bfd392b56df341eeebd75e91794f98c70d1900
MurfiqahSHH <murfiqah.raini@senghoehuat.com>
2023-02-10 14:09:56 +0800

    commit changes for home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,22 @@
 <template>
     <LayoutWrapper>
-        Home Page
+        <template v-if="shownColor == 'red'">
+            its gred
+        </template>
+        <template v-else>
+            <div class="flex flex-row w-full justify-around">
+            <div class="flex h-20 bg-red-400">123</div>
+            <div class="flex h-20 bg-blue-500">123</div>
+            <div class="flex h-20 bg-green-600">123</div>
+            <div class="flex h-20 bg-yellow-300">123</div>
+        </div>
+        </template>
+        
     </LayoutWrapper>
 </template>
 
 <script setup lang="ts">
 import LayoutWrapper from '@/components/Layout/LayoutWrapper.vue';
 
+const shownColor = 'someth'
 </script>
