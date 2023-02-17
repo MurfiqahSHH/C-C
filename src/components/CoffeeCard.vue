f956051bf393d57d3b462def89b8e5bc204bea04
Gregg Redman <greggredman01@gmail.com>
2023-02-17 14:45:33 +0800

    data 2

---
+++ src/components/CoffeeCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col p-4 rounded-lg bg-white shadow-lg">
+    <div class="flex flex-row"><img src="" alt=""></div>
+    <div class="flex flex-row">Title</div>
+    <div class="flex flex-row">Description</div>
+    <div class="flex flex-row">price</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  item: {
+    type: Object
+  },
+});
+</script>
--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,8 +1,34 @@
 <template>
-    <LayoutWrapper>About</LayoutWrapper>
+  <LayoutWrapper>
+    <div class="flex flex-row">
+      <CoffeeCard />
+      {{ data }}
+    </div>
+  </LayoutWrapper>
 </template>
 
 <script setup lang="ts">
-import LayoutWrapper from '@/components/Layout/LayoutWrapper.vue';
+import CoffeeCard from "@/components/CoffeeCard.vue";
+import LayoutWrapper from "@/components/Layout/LayoutWrapper.vue";
 
+const data = [
+  {
+    name: "coffee1",
+    price: 5.99,
+    description: "This is coffee 1",
+    image: "https://www.fillmurray.com/640/360",
+  },
+  {
+    name: "coffee2",
+    price: 7.99,
+    description: "This is coffee 2",
+    image: "https://www.fillmurray.com/640/700",
+  },
+  {
+    name: "coffee3",
+    price: 8.99,
+    description: "This is coffee 1",
+    image: "https://www.fillmurray.com/640/400",
+  },
+];
 </script>
